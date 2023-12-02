ff8704eb23906cd5b00ec1af5f1134fdd353347f
Andrew Kurylyk <andronniy7@gmail.com>
2023-12-02 23:11:22 +0200

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,22 @@ import { HttpClientModule } from "@angular/common/http";
 import {UsersComponent} from "./components/users/users.component";
 
 @NgModule({
-
   declarations: [
     AppComponent,
     UsersComponent,
     UserComponent
   ],
-
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-
   providers: [],
-  exports: [
-    UsersComponent
-  ],
   bootstrap: [AppComponent]
-
 })
 
 export class AppModule { }
+
+
 
 
 
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,17 +1,10 @@
-
-
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {UserComponent} from "./user/user.component";
-import {AppModule} from "../app.module";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent, AppModule, AppModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'AngProj';
